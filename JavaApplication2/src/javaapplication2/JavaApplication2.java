04d11164c29b7b14a787dbf76228ce836556f2c8
Arinnka <Delena315@yandex.ru>
2017-05-30 21:58:37 +0300

    end

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -7,6 +7,7 @@ package javaapplication2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -21,108 +22,84 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     
-    public static final String alb = "abcdefghijklmnopqrstuvwxyz";
-   static String[][] generateMap(char[] alphabet, char[] key, char[] message) {
-        int count = 1;
-        double heighttmp = message.length / key.length;
-        int height = (int) (Math.ceil(heighttmp) + 2);
-        String[][] map = new String[height][key.length];
-        for (int i = 0; i < key.length; i++) {
-            map[0][i] = String.valueOf(key[i]);
-        }
-        for (char c : alphabet) {
-            for (int i = 0; i < key.length; i++) {
-                if (c == key[i]) {
-                    map[1][i] = Integer.toString(count);
-                    count++;
-                }
-            }
-        }
-        count = 0;
-        for (int i = 2; i < height; i++) {
-            for (int j = 0; j < key.length; j++) {
+   static String[][] generateMap(char[] alphabet, char[] key, char[] message) { 
+int count = 1; 
+double heighttmp = message.length * 1.0 / key.length ; 
+int height = (int) (Math.ceil(heighttmp) + 2); 
+String[][] map = new String[height][key.length]; 
+for (int i = 0; i < key.length; i++) { 
+map[0][i] = String.valueOf(key[i]); 
+} 
+for (char c : alphabet) { 
+for (int i = 0; i < key.length; i++) { 
+if (c == key[i]) { 
+map[1][i] = Integer.toString(count); 
+count++; 
+} 
+} 
+} 
+count = 0; 
+for (int i = 2; i < height; i++) { 
+for (int j = 0; j < key.length; j++) { 
 
-                if (count < message.length) {
-                    map[i][j] = String.valueOf(message[count]);
-                    count++;
-                } else {
-                    map[i][j] = "";
-                }
-            }
-        }
-        return map;
-    }
+if (count < message.length) { 
+map[i][j] = String.valueOf(message[count]); 
+count++; 
+} else { 
+map[i][j] = ""; 
+} 
+} 
+} 
+return map; 
+} 
 
- public static boolean chkey(String keytmp) {
-        for (int l = 0; l < keytmp.length(); l++) {
-            if (!(alb.contains(String.valueOf(keytmp.charAt(l))))) {
-                System.out.println("Key string may contain only lowercase letters");
-                return false;
-            }
+public static void main(String[] args) throws UnsupportedEncodingException, IOException { 
+String alb = "АБВГДЕЖЗИЙКЛМНОПРСТУФХШЩЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюя., :-"; 
+char[] alphabet = alb.toCharArray(); 
+BufferedReader in = new BufferedReader(new InputStreamReader(System.in,"Cp1251")); 
 
-        }
-        return true;
-    }
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-       
-        char[] alphabet = alb.toCharArray();
-        Scanner in = new Scanner(System.in);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("Enter message");
-        String messagetmp = in.nextLine();
-        char[] message = messagetmp.toCharArray();
+System.out.println("Enter message"); 
+String messagetmp = in.readLine(); 
+char[] message = messagetmp.toCharArray(); 
 
-        System.out.println("Enter key");
-        String keytmp = "";
-        try {
-            do{
-                        System.out.println("Key string may contain only lowercase letters");
-            keytmp = reader.readLine();
-        }
-            while(!chkey(keytmp));
-        } catch (IOException ex) {
-            Logger.getLogger(JavaApplication2.class.getName()).log(Level.SEVERE, null, ex);
-        }
+System.out.println("Enter the key"); 
+String keytmp = in.readLine(); 
 
-        if (keytmp.length() > message.length) {
-            int difference = keytmp.length() - message.length;
-            keytmp = keytmp.substring(0, keytmp.length() - difference);
-        }
+if (keytmp.length() > message.length) { 
+int difference = keytmp.length() - message.length; 
+keytmp = keytmp.substring(0, keytmp.length() - difference); 
+} 
 
-        char[] key = keytmp.toCharArray();
+char[] key = keytmp.toCharArray(); 
 
-        String[][] map = generateMap(alphabet, key, message);
-        for (String[] c : map) {
-            System.out.println(Arrays.toString(c));
-        }
+String[][] map = generateMap(alphabet, key, message); 
+for (String[] c : map) { 
+System.out.println(Arrays.toString(c)); 
+} 
 
-        int count = 1;
-        double heighttmp = message.length / key.length;
-        int height = (int) (Math.ceil(heighttmp) + 2);
-        int stolbec;
-        System.out.print("Encoded message");
-        for (int j = 0; j < key.length; j++) {
-            stolbec = Arrays.asList(map[1]).indexOf(String.valueOf(count));
-            for (int i = 2; i < height; i++) {
-                System.out.print(map[i][stolbec]);
-            }
-            count++;
-        }
+int count = 1; 
+double heighttmp = message.length * 1.0 / key.length ; 
+int height = (int) (Math.ceil(heighttmp) + 2); 
+int stolbec; 
+System.out.print("And the encoded message is ... "); 
+for (int j = 0; j < key.length; j++) { 
+stolbec = Arrays.asList(map[1]).indexOf(String.valueOf(count)); 
+for (int i = 2; i < height; i++) { 
+System.out.print(map[i][stolbec]); 
+} 
+count++; 
+} 
 
-        System.out.println("\n");
-        System.out.print("Decoded message:");
-        for (int i = 2; i < height; i++) {
-            for (int j = 0; j < key.length; j++) {
-                System.out.print(map[i][j]);
-            }
-        }
-        System.out.println("\n");
-    }
-}
-
+System.out.println("\n"); 
+System.out.print("And the decoded message is ... "); 
+for (int i = 2; i < height; i++) { 
+for (int j = 0; j < key.length; j++) { 
+System.out.print(map[i][j]); 
+} 
+} 
+System.out.println("\n");
+}}
     
     
 
